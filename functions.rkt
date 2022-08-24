f8022c836acccf1197d9c0bafdc423049a8b7fd6
aveeeeen <taichimatsumoto360@gmail.com>
2022-08-24 15:39:39 +0900

    first commit

---
+++ functions.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname functions) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(define (dist x y)
+  (sqrt
+   (+ (* x x)(* y y))
+  )
+)
+
+(dist 3 4)
+
+(write-file 'stdout "212\n")
+
+
