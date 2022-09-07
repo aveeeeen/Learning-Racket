5fb69e164a0e4ad8f9ba9370f7396527d2a56c1d
aveeeeen <taichimatsumoto360@gmail.com>
2022-09-07 19:50:50 +0900

    week3

--- week2(chap2).rkt
+++ week2(chap2).rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |week2(chap2)|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |week2(chap2)|) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
 ; exercise 13: Define the function string-first, which extracts the first 1String from a non-empty string;
 ; String -> 1String
 ; Result expects:
@@ -17,3 +17,4 @@
 
 (define (==> sunny friday)
   (or sunny friday))
+
--- week3(chap2).rkt
+++ week3(chap2).rkt
@@ -1,23 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |week2(chap2)|) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
-; exercise 13: Define the function string-first, which extracts the first 1String from a non-empty string;
-; String -> 1String
-; Result expects:
-; (string-first "hello") retruns "h"
-; (string-first "") returns ""
-
-(define (string-first str)
-  (if (string=? str "")
-      ""
-      (string-ith str 0)))
-
-; exercize 15: Define ==>
-; bool bool -> bool
-
-(define (==> sunny friday)
-  (or sunny friday))
-
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |week3(chap2)|) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
 ;-----------------------------------------------------------------
 ;week 3 starts from here
 
